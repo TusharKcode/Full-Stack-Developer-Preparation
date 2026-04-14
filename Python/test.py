d2ce3450c704ac64de54fa7e8bb1030713a72b62
Tushar Kumar <tkgoyal104@gmail.com>
2026-04-14 11:36:27 +0530

    test Py

---
+++ Python/test.py
@@ -0,0 +1,6 @@
+nums = [1, 2, 3, 4, 5]
+result = [(x, x**2) for x in nums if x % 2 != 0]
+print(result)
+
+x = int(input("Enter x: "))
+print("A" if x > 10 and x % 2 == 0 else "B" if x > 10 else "C")
