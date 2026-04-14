f612d27b0b17de50e7ac400bc947996f8284e891
Tushar Kumar <tkgoyal104@gmail.com>
2026-04-14 18:30:51 +0530

    test py

--- Python/test.py
+++ Python/test.py
@@ -4,3 +4,6 @@ print(result)
 
 x = int(input("Enter x: "))
 print("A" if x > 10 and x % 2 == 0 else "B" if x > 10 else "C")
+
+data = ["apple","cat","banana","car","dog"]
+print(sorted(data, key=lambda x:(len(x)%2, -len(x), x), reverse=True))
