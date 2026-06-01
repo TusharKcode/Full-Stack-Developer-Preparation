06f08bd4c9c3f9d0aff8a07b0006ac1900aaf43d
Tushar Kumar <tkgoyal104@gmail.com>
2026-06-01 11:21:00 +0530

    MongoDB- Query, Object-Id, JSON-BSON

---
+++ Database/MongoDB/Basics/query-op.sql
@@ -0,0 +1,66 @@
+[
+    {
+        "_id": ObjectId("6009585d35cce6b7b8f087f1"),
+        "title": "Math",
+        "author": "Alen",
+        "level": "basic",
+        "length": 230,
+        "example": 11
+    },
+    {
+        "_id": ObjectId("60095b8a3fc110f90873ce29"),
+        "title": "Array",
+        "author": "Alen",
+        "level": "basic",
+        "length": 200,
+        "example": 5
+    },
+    {
+        "_id": ObjectId("60095b8a3fc110f90873ce2a"),
+        "title": "Stack",
+        "author": "Rim",
+        "level": "easy",
+        "length": 400,
+        "example": 10
+    },
+    {
+        "_id": ObjectId("60095b8a3fc110f90873ce2b"),
+        "title": "Queue",
+        "author": "Rim",
+        "level": "medium",
+        "length": 350,
+        "example": 2
+    },
+    {
+        "_id": ObjectId("60095b8a3fc110f90873ce2c"),
+        "title": "Tree",
+        "author": "devil",
+        "level": "high",
+        "length": 1000,
+        "example": 10
+    },
+    {
+        "_id": ObjectId("60095b8a3fc110f90873ce2d"),
+        "title": "Graph",
+        "author": "Alen",
+        "level": "high",
+        "length": 1500,
+        "example": 15
+    },
+    {
+        "_id": ObjectId("60095b8d3fc110f90873ce2e"),
+        "title": "Segment Tree",
+        "author": "devil",
+        "level": "very high",
+        "length": 500,
+        "example": 20
+    },
+    {
+        "_id": ObjectId("60095fd4e5a7731b2a55a922"),
+        "title": "Trie",
+        "author": "Rim",
+        "length": 500,
+        "example": 20,
+        "time": 50
+    }
+]
