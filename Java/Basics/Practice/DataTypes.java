48e276e265095823d278286141d29e93e77416fa
Tushar Kumar <tkgoyal104@gmail.com>
2026-06-28 11:25:00 +0530

    Java - Intro, Identifiers-Keywords, Data Types, Jdk-Jre-Jvm, Varaibles, Conditional Statements, Operators

---
+++ Java/Basics/Practice/DataTypes.java
@@ -0,0 +1,53 @@
+package Practice;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        System.out.println("------ Boolean Data Type ------");
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is fish tasty? " + isFishTasty);
+
+        System.out.println("------ Byte Data Type ------");
+        byte age = 25;
+        byte temperature = -10;
+        System.out.println("Age: " + age);
+        System.out.println("Temperature: " + temperature);
+
+        System.out.println("------ Short Data Type ------");
+        short students = 1000;
+        short temp = -200;
+        System.out.println("Number's of Students: " + students);
+        System.out.println("Temperature: " + temp);
+
+        System.out.println("------ Int Data Type ------");
+        int population = 2000000;
+        int distance = 150000000;
+        System.out.println("Population: " + population);
+        System.out.println("Distance: " + distance);
+
+        System.out.println("------ Long Data Type ------");
+        long worldPopulation = 7800000000L;
+        long lightYears = 9460730472580800L;
+        System.out.println("World Population: " + worldPopulation);
+        System.out.println("Light Years: " + lightYears);
+
+        System.out.println("------ Float Data Type ------");
+        float pi = 3.14f;
+        float gravity = 9.81f;
+        System.out.println("Pi: " + pi);
+        System.out.println("Gravity: " + gravity);
+
+        System.out.println("------ Double Data Type ------");
+        double doublePI = 3.141592653589793;
+        double avogadro = 6.02214076e23;
+        System.out.println("Pi: " + doublePI);
+        System.out.println("Avogadro's Number: " + avogadro);
+
+        System.out.println("------ Char Data Type ------");
+        char grade = 'A';
+        char symbol = '$';
+        System.out.println("Grade: " + grade);
+        System.out.println("Symbol: " + symbol);
+    }
+}
